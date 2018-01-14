80e5e0d47a2a225e6608defd9a5cfa74261c784d
Dylan Ferrara <dylan.ferrara@gmail.com>
2018-01-14 23:27:59 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -3,8 +3,12 @@ var playlist = {
 };
 
 function updatePlaylist(playlist, name, song) {
+<<<<<<< HEAD
   //playlist = Object.assign({}, playlist, {[name]: song});
   playlist[name] = song;
+=======
+  playlist = Object.assign({}, playlist, {[name]: song});
+>>>>>>> c5f4df4f0ef8fb7c9097df6a7779f1fea3cdd247
   return playlist;
 }
 
